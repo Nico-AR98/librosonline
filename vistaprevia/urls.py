b5cc00222429b116b0b3d93fe9fe84297c10c55f
Nico Gallardo <alfredongallg@gmail.com>
2022-12-26 19:43:58 -0300

    first commit

--- vistaprevia/urls.py
+++ vistaprevia/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from vistaprevia import views
+from vistaprevia.views import Templatetags1
+from vistaprevia.views import BuscarLibro
+from vistaprevia.views import BuscarLibro2
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("templatetags1", Templatetags1.as_view(), name="templatetags1"),
+    path("usar_ajax", views.para_ajax, name="usar_ajax"),
+    path("buscar_libro/", BuscarLibro.as_view(), name="buscar_libro"),
+    path("buscar_libro2/", BuscarLibro2.as_view(), name="buscar_libro2"),
 ]
